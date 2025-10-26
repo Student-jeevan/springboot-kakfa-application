ea29f8b4104e3b9555faf66688757227129484c3
jeevansingh <jeevanthitrya@gmail.com>
2025-10-26 11:44:34 +0530

    multiple consumers added

--- producer/src/main/java/com/Kafka/producer/FunctionClass.java
+++ producer/src/main/java/com/Kafka/producer/FunctionClass.java
@@ -1,4 +1,14 @@
 package com.Kafka.producer;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
 public class FunctionClass {
+    @Bean
+    public Function<String , String> uppercase(){
+        return String::toUpperCase;
+    }
 }
